2817d672a425fe2ce6272d06ed769e11bf8301b3
Dale Eidd <dale@eidd.name>
2021-12-26 15:34:03 -0800

    Demo: Add test data

---
+++ crest/Assets/Demo/DebugFloatingOrigin.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Crest;
+[ExecuteAlways]
+public class DebugFloatingOrigin : MonoBehaviour
+{
+    FloatingOrigin m_FloatingOrigin;
+    [SerializeField]
+    bool m_UpdatePosition = false;
+    [SerializeField]
+    float m_PositionOffset = 0f;
+
+    void Start()
+    {
+        m_FloatingOrigin = GetComponent<FloatingOrigin>();
+    }
+
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    void UpdatePosition()
+    {
+        if (Application.isPlaying)
+        {
+            return;
+        }
+
+        if (!m_UpdatePosition)
+        {
+            return;
+        }
+
+        var position = transform.position;
+        position.x = position.z = Mathf.Floor(m_PositionOffset / m_FloatingOrigin._threshold) * m_FloatingOrigin._threshold + m_FloatingOrigin._threshold - 0.15f;
+        transform.position = position;
+    }
+}
