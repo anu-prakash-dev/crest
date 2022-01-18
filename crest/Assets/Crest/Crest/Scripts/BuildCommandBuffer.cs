8397069fdc699dcea0ef855533686500243555e5
Dale Eidd <dale@eidd.name>
2022-01-17 16:34:04 -0800

    Fix non mesh renderer inputs flickering
    Thanks to Discord user scrappy

--- crest/Assets/Crest/Crest/Scripts/BuildCommandBuffer.cs
+++ crest/Assets/Crest/Crest/Scripts/BuildCommandBuffer.cs
@@ -111,6 +111,12 @@ namespace Crest
 
             BuildLodData(OceanRenderer.Instance, _buf);
 
+            if (OceanRenderer.Instance.ViewCamera != null)
+            {
+                // Fixes glitching trail renderers. Method is undocumented.
+                Camera.SetupCurrent(OceanRenderer.Instance.ViewCamera);
+            }
+
             // This will execute at the beginning of the frame before the graphics queue
             Graphics.ExecuteCommandBuffer(_buf);
 
